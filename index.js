6e4eb4550d0aad48f294c02bc455ed62429928ca
Tim <timmymclennan@gmail.com>
2024-02-12 12:48:57 +1100

    Initialised the earth model and imported the associated ThreeJS library.

--- index.js
+++ index.js
@@ -1,3 +1,8 @@
+import * as THREE from "three";
+import { OrbitControls } from 'jsm/controls/OrbitControls.js';
+
+import getStarfield from "./src/getStarfield.js";
+import { getFresnelMat } from "./src/getFresnelMat.js";
 
 // Scene setup: 
 // The following lines define the scene dimensions (w and h) and initialize the scene, camera, and renderer. 
@@ -12,3 +17,20 @@ const renderer = new THREE.WebGLRenderer({ antialias: true });
 renderer.setSize(w, h);
 document.body.appendChild(renderer.domElement);
 
+// Earth group creation: 
+// I created a 'THREE.Group' instance named earthGroup here to hold all objects related to Earth, including the Earth sphere itself, its glow effect, and atmospheric effects. 
+// The rotation of this group is adjusted to account for the tilt of the Earth's axis.
+const earthGroup = new THREE.Group();
+earthGroup.rotation.z = -23.4 * Math.PI / 180;
+scene.add(earthGroup);
+new OrbitControls(camera, renderer.domElement);
+
+// Textures loading:
+// A THREE.TextureLoader instance is created to load textures for the Earth model, including the surface texture, specular map, bump map, and others for the glow and cloud effects.
+const detail = 12;
+const loader = new THREE.TextureLoader();
+
+// Earth model creation: 
+// A THREE.IcosahedronGeometry is created for the Earth sphere with a specified level of detail. 
+// A THREE.MeshPhongMaterial is then applied to the geometry to give it a realistic appearance with lighting. 
+// The material properties include maps for textures and bump scaling.
